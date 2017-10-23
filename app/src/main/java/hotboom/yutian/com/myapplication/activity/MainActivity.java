7c13be5fa87a05ae3779e901f4646f92cf36a065
wang <592357390@qq.com>
2017-10-23 20:41:52 +0800

    Initial commit

---
+++ app/src/main/java/hotboom/yutian/com/myapplication/activity/MainActivity.java
@@ -0,0 +1,85 @@
+package hotboom.yutian.com.myapplication.activity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hotboom.yutian.com.myapplication.R;
+import hotboom.yutian.com.myapplication.adapter.MainPagerAdapter;
+import hotboom.yutian.com.myapplication.databinding.ActivityMainBinding;
+import hotboom.yutian.com.myapplication.fragment.CustomerFragment;
+import hotboom.yutian.com.myapplication.fragment.GoodsFragment;
+import hotboom.yutian.com.myapplication.fragment.OrderFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fragment> fragments = new ArrayList<>();
+
+    private List<String> titles = new ArrayList<>();
+
+    private List<Integer> imgIds = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        fragments.add(new OrderFragment());
+        fragments.add(new CustomerFragment());
+        fragments.add(new GoodsFragment());
+
+        titles.add("订单");
+        titles.add("客户");
+        titles.add("商品");
+
+        imgIds.add(R.drawable.order_navigation);
+        imgIds.add(R.drawable.customer_navigation);
+        imgIds.add(R.drawable.goods_navigation);
+
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.viewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(), fragments));
+
+        for (int i = 0; i < fragments.size(); i++) {
+            TabLayout.Tab tab = binding.tabLayout.newTab();
+
+            View navigationBtnLayout = LayoutInflater.from(this).inflate(R.layout.main_navigation_btn, null);
+            TextView navigationBtn = navigationBtnLayout.findViewById(R.id.navigation_btn);
+            navigationBtn.setText(titles.get(i));
+            navigationBtn.setCompoundDrawables(null, ContextCompat.getDrawable(this, imgIds.get(i)), null, null);
+
+            tab.setCustomView(navigationBtn);
+            binding.tabLayout.addTab(tab);
+        }
+
+        binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                TabLayout.Tab tab = binding.tabLayout.getTabAt(position);
+                if (tab != null && !tab.isSelected()) {
+                    tab.select();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+//        binding.tabLayout.setupWithViewPager(binding.viewPager);
+    }
+}
---
+++ app/src/main/java/hotboom/yutian/com/myapplication/adapter/MainPagerAdapter.java
@@ -0,0 +1,43 @@
+package hotboom.yutian.com.myapplication.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zx on 2017/10/23 0023.
+ */
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+
+
+    private List<Fragment> fragments = new ArrayList<>();
+
+    private List<String> titles = new ArrayList<>();
+
+    private List<Integer> imgIds = new ArrayList<>();
+
+    public MainPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+
+        this.fragments = fragments;
+
+        titles.add("订单");
+        titles.add("客户");
+        titles.add("商品");
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+}
